81931e7d43a155a3bf702a61ee22340b5ef1f08e
timmyhoa <timmyhoa34@gmail.com>
2022-09-13 17:16:11 -0400

    done

--- src/App.js
+++ src/App.js
@@ -5,51 +5,50 @@ let clickedImage = [];
 function App() {
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
-  const [imageOrder, setImageOrder] = useState([...Array(10).keys()]);
+  const [images, setImage] = useState([
+    {
+      imageName: "Tom",
+      link: "./image/1.jpg",
+      id: 1,
+    },
+    {
+      imageName: "Tim",
+      link: "./image/2.jpg",
+      id: 2,
+    },
+    {
+      imageName: "Tam",
+      link: "./image/3.jpg",
+      id: 3,
+    },
+    {
+      imageName: "John",
+      link: "./image/4.jpg",
+      id: 4,
+    },
+    {
+      imageName: "Alex",
+      link: "./image/5.jpg",
+      id: 5,
+    },
+    {
+      imageName: "Oliver",
+      link: "./image/6.jpg",
+      id: 6,
+    },
+    {
+      imageName: "Alexa",
+      link: "./image/7.jpg",
+      id: 7,
+    },
+    {
+      imageName: "Cat",
+      link: "./image/8.jpg",
+      id: 8,
+    },
+  ]);
 
   function loadImage() {
-    const images = [
-      {
-        imageName: "Tom",
-        link: "./image/1.jpg",
-        id: 1,
-      },
-      {
-        imageName: "Tim",
-        link: "./image/2.jpg",
-        id: 2,
-      },
-      {
-        imageName: "Tam",
-        link: "./image/3.jpg",
-        id: 3,
-      },
-      {
-        imageName: "John",
-        link: "./image/4.jpg",
-        id: 4,
-      },
-      {
-        imageName: "Alex",
-        link: "./image/5.jpg",
-        id: 5,
-      },
-      {
-        imageName: "Oliver",
-        link: "./image/6.jpg",
-        id: 6,
-      },
-      {
-        imageName: "Alexa",
-        link: "./image/7.jpg",
-        id: 7,
-      },
-      {
-        imageName: "Cat",
-        link: "./image/8.jpg",
-        id: 8,
-      },
-    ];
     return (
       <div className="container text-center">
         <div className="row">
@@ -78,7 +77,7 @@ function App() {
     }
     clickedImage.push(imageId);
     setScore(score + 1);
-    shuffle(clickedImage);
+    setImage(shuffle([...images]));
   }
 
   function updateBestScore() {
@@ -113,6 +112,8 @@ function shuffle(array) {
       array[randomIndex],
       array[currentIndex],
     ];
+
+    return array;
   }
 }
 
